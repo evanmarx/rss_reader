fc4f14fbc945e024a4c6ab72fa89fad60951643c
Angelo and James <angelo+james@appacademy.io>
2013-05-28 09:13:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsReader::Application.config.secret_token = 'c0d5c7f87fd1710ebb7f97641ed7025d250c04c48853e3b12f4f89d10604c571f9d8128681de814d02dcb954f4228171d432e5b3276100d92030685bad2accb5'
